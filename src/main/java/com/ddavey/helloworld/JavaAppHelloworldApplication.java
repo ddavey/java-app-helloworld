1a57ab687dd73af894b3654441c5b7742aef473d
Dan Davey <daniel.davey@live.co.uk>
2023-07-04 18:55:55 +0100

    Initial commit

---
+++ src/main/java/com/ddavey/helloworld/JavaAppHelloworldApplication.java
@@ -0,0 +1,15 @@
+package com.ddavey.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAppHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAppHelloworldApplication.class, args);
+	}
+	
+	
+
+}
---
+++ src/test/java/com/ddavey/helloworld/JavaAppHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddavey.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
